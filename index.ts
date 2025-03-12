897e31766081da23bd215549d5be1b0d4934c6a3
dakota002 <dakota.d@live.com>
2025-03-12 16:38:14 -0400

    And in the other spot

--- index.ts
+++ index.ts
@@ -49,7 +49,11 @@ export const sandbox = {
         requestTimeout: 10_000,
         httpsAgent: { maxSockets: 500 }, // Increased from default to allow for higher throughput
       },
-      credentials,
+      credentials: {
+        // Any credentials can be provided for local
+        accessKeyId: 'local-db',
+        secretAccessKey: 'random-any-string',
+      },
     })
     const seedFile = arc['architect-plugin-dynamodb-local'].find(
       (item: string[]) => item[0] == 'seedFile'
