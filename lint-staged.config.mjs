86fcf680f7b1290d4569e6521d54a90ff0aa27a3
dakota002 <dakota.d@live.com>
2025-03-27 11:10:20 -0400

    Just reuse the command

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,5 @@
 export default {
   '*.(json|md|ts|mjs)': 'prettier --write',
   '*.(ts|mjs)': 'eslint --max-warnings 0 .',
-  '*.ts': () =>
-    'esbuild index.ts --bundle --packages=external --outfile=index.js --platform=node --format=esm --tree-shaking=true',
+  '*.ts': () => 'prepare:esbuild',
 }
